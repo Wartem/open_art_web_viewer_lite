ef3afec87ff25b11de7673d1b8e8a3cb77f185c5
Wartem <mwasteson@gmail.com>
2024-08-26 16:44:31 +0200

    Heroku Deployment
    Heroku Deployment

--- open_art_web_viewer_lite/app copy.py
+++ open_art_web_viewer_lite/app copy.py
@@ -1,18 +1,26 @@
 import json
 import os
+import importlib
 import sys
 
-# Add the project root directory to sys.path
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
+print("Current working directory:", os.getcwd())
+print("sys.path:", sys.path)
 
-# Print debug information
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+    
 print("Current working directory:", os.getcwd())
 print("sys.path:", sys.path)
 
 # Read the project name from the config file
-config_path = os.path.join(project_root, 'config.json')
+config_path = os.path.join(parent_dir, 'config.json')
+
+#if parent_dir not in sys.path:
+    #sys.path.insert(0, parent_dir)
 
 try:
     with open(config_path, 'r') as config_file:
@@ -21,8 +29,13 @@ try:
 
     if not project_name:
         raise ValueError("Project name not found in config file")
-
-    # Import the create_app function
+    
+    # Dynamic import using importlib
+    #module = importlib.import_module(project_name)
+    
+    #module = importlib.import_module('.' + project_name, package=__package__)
+    #create_app = getattr(module, 'create_app')
+    
     from open_art_web_viewer_lite import create_app
 
     app = create_app()
@@ -40,7 +53,10 @@ except json.JSONDecodeError as e:
 except ValueError as e:
     print(f"Error: {str(e)}")
 except ImportError as e:
-    print(f"Error: Could not import create_app function from '{project_name}'. Make sure it exists and is in the correct location.")
+    print(f"Error: Could not import module '{project_name}'. Make sure it exists and is in the correct location.")
+    print(f"Error: {str(e)}")
+except AttributeError as e:
+    print(f"Error: Module '{project_name}' does not have a 'create_app' function.")
     print(f"Error: {str(e)}")
 except Exception as e:
     print(f"An unexpected error occurred: {str(e)}")
